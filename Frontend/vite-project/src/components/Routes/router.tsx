6db1966632e05759d1bc2589cc909a3895e4834f
Nimesh Madhusanka <madhusankan19@gmail.com>
2025-06-01 01:03:54 +0530

    Add responsive Signup  page with form and styling

--- Frontend/vite-project/src/components/Routes/router.tsx
+++ Frontend/vite-project/src/components/Routes/router.tsx
@@ -4,12 +4,14 @@ import Hero from '../LandingPage/Hero.tsx';
 import Category from '../LandingPage/Category.tsx';
 import Footer from '../Footer/footer.tsx';
 import Login from '../Login/login.tsx';
+import Signup from '../SingUp/signup.tsx';
 const router = () => {
   return (
     <Router>
         <Routes>
             <Route path="/" element={<><NavBar/><Hero/><Category/><Footer/></>} />
             <Route path="/login" element={<><Login/></>} />
+            <Route path="/register" element={<><Signup/></>} />
         </Routes>
     </Router>
   )
