45defaaaf2eb188838199c92d009ac3b720ffb92
WeerasingheMSC <sahanchampathi2001ugc@gmail.com>
2025-05-31 22:56:34 +0530

    add Topselling components
    All component are responsive even mobile view

--- Frontend/vite-project/src/components/Routes/router.tsx
+++ Frontend/vite-project/src/components/Routes/router.tsx
@@ -3,11 +3,12 @@ import NavBar from '../Navbar/NavBar.tsx';
 import Hero from '../LandingPage/Hero.tsx';
 import Category from '../LandingPage/Category.tsx';
 import Footer from '../Footer/footer.tsx';
+import TopSelling from '../LandingPage/topSelling.tsx';
 const router = () => {
   return (
     <Router>
         <Routes>
-            <Route path="/" element={<><NavBar/><Hero/><Category/><Footer/></>} />
+            <Route path="/" element={<><NavBar/><Hero/><Category/><TopSelling/><Footer/></>} />
         </Routes>
     </Router>
   )
