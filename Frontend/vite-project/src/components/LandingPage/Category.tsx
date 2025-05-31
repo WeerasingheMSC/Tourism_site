d40e6693fa174e7894c5c19ad8576fff807db5ad
WeerasingheMSC <sahanchampathi2001ugc@gmail.com>
2025-05-31 15:22:58 +0530

    add image for category section

--- Frontend/vite-project/src/components/LandingPage/Category.tsx
+++ Frontend/vite-project/src/components/LandingPage/Category.tsx
@@ -4,11 +4,12 @@ import Group48 from "../../assets/Group 48.png";
 import Group49 from "../../assets/Group 49.png";
 import Group50 from "../../assets/Group 50.png";
 import Group51 from "../../assets/Group 51.png";
+import Rectangle from "../../assets/Rectangle 157.png";
 import { Row, Col } from "antd";
 
 const Category = () => {
   return (
-    <div className="max-w-screen-xl mx-auto px-4 mt-12">
+    <div className="max-w-screen-xl mx-auto px-4 mt-12 relative">
       <div className="relative">
         <img src={Group4} alt="" className="hidden md:flex absolute right-0 w-40 h-50 md:w-52 md:h-52 z-10" />
       </div>
@@ -21,20 +22,36 @@ const Category = () => {
           CATEGORY
         </h1>
         <h1 className="text-center text-2xl md:text-5xl font-serif font-semibold mt-2">
-          We offer best service
+          We Offer Best Services
         </h1>
       </div>
 
       <Row gutter={[16, 24]} justify="center">
         {[ 
-          { img: Group48, title: "Travelling Partner", desc: "Built Wicket longer admire do barton vanity itself do in it." },
-          { img: Group51, title: "Best Flights", desc: "Built Wicket longer admire do barton vanity itself do in it." },
-          { img: Group50, title: "Hotel Partner", desc: "Built Wicket longer admire do barton vanity itself do in it." },
-          { img: Group49, title: "Customization", desc: "Built Wicket longer admire do barton vanity itself do in it." },
+          { 
+            img: Group48, 
+            title: "Travelling Partner", 
+            desc: "Built  Wicket  longer  admire  do  barton  vanity  itself  do  in  it.........." 
+          },
+          { 
+            img: Group51, 
+            title: "Best Flights", 
+            desc: "Engrossed listening. Park gate sell they west hard for the........." 
+          },
+          { 
+            img: Group50, 
+            title: "Hotel Partner", 
+            desc: "Barton vanity itself do in it. Preferd to men it engrossed listening." 
+          },
+          { 
+            img: Group49, 
+            title: "Customization", 
+            desc: "We deliver outsourced aviation services for military customers....." 
+          },
         ].map(({ img, title, desc }, idx) => (
           <Col key={idx} xs={24} sm={12} md={8} lg={6}>
             <div
-              className="relative bg-white p-6 md:mt-40 mt-2 w-100 ml-1 w-fit md:w-full rounded-xl shadow-lg border-2 border-amber-50 transform transition-transform duration-300 hover:-translate-y-2 hover:shadow-xl cursor-pointer"
+              className="relative bg-white p-6 md:mt-40 z-10 mt-2 w-100 ml-1 w-fit md:w-full rounded-xl shadow-lg border-2 border-amber-50 transform transition-transform duration-300 hover:-translate-y-2 hover:shadow-xl cursor-pointer"
               style={{ borderRadius: "10%" }}
             >
              
@@ -49,7 +66,9 @@ const Category = () => {
             </div>
           </Col>
         ))}
+        <img src={Rectangle} alt="" className="absolute hidden lg:flex top-118 left-82 z-0 " />
       </Row>
+
     </div>
   );
 };
--- Frontend/vite-project/src/components/LandingPage/Hero.tsx
+++ Frontend/vite-project/src/components/LandingPage/Hero.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
             <p className='text-orange-600 text-xs text-center md:text-lg font-extrabold font-popins md:text-start lg:text-start'>BEST DESTINATION AROUND SRI LANKA</p>
             <p className='text-4xl md:text-7xl w-110 font-extrabold mt-8 text-center md:text-start lg:text-start' style={{fontFamily:"sans-serif"}} >Travel, enjoy and live a new and full life</p>
             <p className='w-110 text-sm md:text-lg text-center font-sans md:text-center lg:text-start'>Built Wicket longer admire do barton vanity itself do in it. Preferred to sportsmen it engrossed listening. Park gate sell they west hard for the.</p>
-            <div className='mt-10 ml-10'>
+            <div className='mt-10 md: ml-0'>
                 <Button type='primary' style={{height:45, width:150}}>Find Out More</Button>
                 <Button style={{height:45, marginLeft:10}}>Customize your plane</Button>
             </div>
