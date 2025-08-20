a7b4580507d28a961643bc5e56e8112593b7b063
KasunShakthi <2001shakthikasun@gmail.com>
2025-08-20 16:03:35 +0530

    Make API

--- Backend/server.js
+++ Backend/server.js
@@ -24,7 +24,7 @@ const app = express();
 
 app.use(express.json()); // parse JSON
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174','http://travelbookingsrilanka.com'],
+  origin: ['http://localhost:5173', 'http://localhost:5174','https://travelbookingsrilanka.com','https://travelbookingsrilanka.com'],
   credentials: true
 })); // enable CORS for frontend
 app.use(morgan("dev")); // request logging
